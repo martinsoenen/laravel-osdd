6244b3ae37af11a57c3f263777a2b2ced83eca38
Martin Soenen <martin.soenen@outlook.fr>
2026-05-19 18:06:49 +0200

    Remove sort from other PR

--- src/Console/Commands/Make/ChoosesOsddLayer.php
+++ src/Console/Commands/Make/ChoosesOsddLayer.php
@@ -38,7 +38,6 @@ trait ChoosesOsddLayer
             options: fn(string $value) => $layers
                 ->filter(fn(Layer $l) => str_contains($l->manifest->name(), $value))
                 ->mapWithKeys(fn(Layer $l) => [$l->manifest->name() => $l->manifest->name()])
-                ->sort()
                 ->all(),
         );
 
